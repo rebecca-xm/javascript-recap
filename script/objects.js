530f8e3d934f833e2436b29518ca1280e9ba62c7
Rebecca <rebecca.mollica@live.it>
2022-12-04 13:06:57 +0100

    add object methods

--- script/objects.js
+++ script/objects.js
@@ -65,3 +65,59 @@ console.log(about);
 
 const challenge = `${about.firstName} has ${about.pets.length} pets, and one of them is called ${about.pets[0]}.`;
 console.log(challenge);
+
+//* OBJECT METHODS
+
+// As we learned, functions are really just another type of value,
+// which means we can create a key-value pair in which the value
+// is a function. So, we can add functions to objects.
+
+const about2 = {
+  firstName: "Rebecca",
+  lastName: "Mollica",
+  birthYear: 1991,
+  job: "developer",
+  pets: ["Pipo", "Pippo", "Benny", "Ercole"],
+  hasDriverLicense: true,
+  //   calcAge: function (birthYear) {
+  //     return 2022 - birthYear;
+  //   },
+
+  //   calcAge: function () {
+  //     return 2022 - this.birthYear;
+  //     // this refers to the current object
+  //     // it helps us respect the DRY principle
+  //   },
+  calcAge: function () {
+    this.age = 2022 - this.birthYear;
+    // we can also use the this keyword to store a new property
+    return this.age;
+  },
+  getSummary: function () {
+    return `${this.firstName} is a ${this.calcAge()} yo ${
+      this.job
+    }, and she has ${
+      this.hasDriverLicense ? "a" : "no"
+    } driver license.`;
+  },
+};
+
+// console.log(about2.calcAge(1991))
+// console.log(about2['calcAge'](1991));
+console.log(about2.calcAge());
+console.log(about2.age);
+console.log(about2.age);
+console.log(about2.age);
+console.log(about2.age);
+// to avoid unnecessary and heavy computation load
+// by calling calcAge just once
+
+// Challenge
+// 'Rebecca is a 31 yo developer, and she has a driver license' but make it dynamic.
+
+// const challenge2 = `${about2.firstName} is a ${about2.calcAge()} yo ${
+//   about2.job
+// }, and she has ${about2.hasDriverLicense ? "a" : "no"} driver license.`;
+// console.log(challenge2);
+
+console.log(about2.getSummary())
